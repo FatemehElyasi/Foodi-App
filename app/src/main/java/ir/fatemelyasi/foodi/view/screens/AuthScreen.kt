966e8fbb1fd570dc582aa2b28e7059ccb96a654a
Fatemelyasi <fatemelyasii@gmail.com>
2024-07-12 23:17:47 +0330

    ADD TableLayout Component

--- app/src/main/java/ir/fatemelyasi/foodi/view/screens/AuthScreen.kt
+++ app/src/main/java/ir/fatemelyasi/foodi/view/screens/AuthScreen.kt
@@ -1,11 +1,28 @@
 package ir.fatemelyasi.foodi.view.screens
 
+import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 
 @Composable
-fun AuthScreen(navController: NavController){
-    Text(text = "Auth Screen")
+fun AuthScreen(navController: NavController) {
+    val context = LocalContext.current
+    val sharedPreferences = context.getSharedPreferences("main", Context.MODE_PRIVATE)
 
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+
+        ) {
+
+    }
 }
